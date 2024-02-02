a3a5501056beb91351347e17802a971795094ac5
RanTempest <151922808+RanTempest@users.noreply.github.com>
2024-02-02 03:28:09 -1200

    Update bbr.sh

--- limit/bbr.sh
+++ limit/bbr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Edition : Stable Edition V3.0
-# Auther  : Geo Project
+# Auther  : RanTempest
 # (C) Copyright 2023
 # =========================================
 
@@ -18,7 +18,7 @@ clear
 #	 menu
 #else
 
-echo -e "Installing TCP BBR Mod By REV VPN STORE"
+echo -e "Installing TCP BBR Mod By RanTempest STORE"
 echo -e "Please Wait BBR Installation Will Starting . . ."
 sleep 5
 clear
