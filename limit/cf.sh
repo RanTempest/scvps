885dfab8ee91c709cdc30ae196129dcf76d1aa8b
RanTempest <151922808+RanTempest@users.noreply.github.com>
2024-02-03 12:30:47 +0700

    Update cf.sh

--- limit/cf.sh
+++ limit/cf.sh
@@ -2,11 +2,11 @@
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
 #read -p "Masukan Domain (contoh : Dragon)" domen
-DOMAIN=klmpk-tunneling.my.id
+DOMAIN=ranstore.cloud
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.klmpk-tunneling.my.id
-CF_ID=andyyuda41@gmail.com
-CF_KEY=0d626234700bad388d6d07b49c42901445d1c
+dns=${sub}.ranstore.cloud
+CF_ID=rantempest2@gmail.com
+CF_KEY=8ec7aedc94db5d4d098f779609a507c8
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
