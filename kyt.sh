d3ccc7bef55b3b42eb6eb0c35b55f2b061ef5ccc
RanTempest <151922808+RanTempest@users.noreply.github.com>
2024-03-02 01:20:28 -1200

    Update kyt.sh

--- kyt.sh
+++ kyt.sh
@@ -7,9 +7,10 @@ grenbo="\e[92;1m"
 NC='\e[0m'
 #install
 apt update && apt upgrade
+apt install neofetch -y
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/RanTempest/scvps/main/bot/bot.zip
+wget https://raw.githubusercontent.com/RanTempest/scvps/main/limit/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
@@ -19,17 +20,20 @@ wget https://raw.githubusercontent.com/RanTempest/scvps/main/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
-#isi data
+clear
 echo ""
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
-echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
-echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -e -p "[*] Input your Bot Token : " bottoken
-read -e -p "[*] Input Your Id Telegram :" admin
+figlet 'TEMPEST' | lolcat
+echo -e "$u ┌────────────────────────────────────────────────┐${NC}"
+echo -e "$u │ \e[1;97;101m                ADD BOT PANEL                 ${NC} ${u}│${NC}"
+echo -e "$u └────────────────────────────────────────────────┘${NC}"
+echo -e "$u ┌────────────────────────────────────────────────┐${NC}"
+echo -e "$u │ ${g}Tutorial Creat Bot and ID Telegram                   ${NC}"
+echo -e "$u │ ${g}Creat Bot and Token Bot : @BotFather                 ${NC}"
+echo -e "$u │ ${g}Info Id Telegram : @MissRose_bot perintah /info      ${NC}"
+echo -e "$u └────────────────────────────────────────────────┘${NC}"
+echo -e ""
+read -e -p "  [*] Input your Bot Token : " bottoken
+read -e -p "  [*] Input Your Id Telegram : " admin
 echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
 echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
 echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
@@ -45,7 +49,7 @@ fi
 
 cat > /etc/systemd/system/kyt.service << END
 [Unit]
-Description=Simple kyt - @kyt
+Description=Simple Bot Tele By - @RanTempest
 After=network.target
 
 [Service]
@@ -57,22 +61,30 @@ Restart=always
 WantedBy=multi-user.target
 END
 
-systemctl daemon-reload
 systemctl start kyt 
 systemctl enable kyt
 systemctl restart kyt
-cd /root
-rm -rf kyt.sh
-echo "Done"
-echo "Your Data Bot"
-echo -e "==============================="
-echo "Token Bot     : $bottoken"
-echo "Admin         : $admin"
-echo "Domain        : $domain"
-echo "Pub           : $PUB"
-echo "Host          : $NS"
-echo -e "==============================="
-echo "Setting done"
-clear
+cd 
 
-echo " Installations complete, type /menu on your bot"
+# // STATUS SERVICE BOT
+bot_service=$(systemctl status xbot | grep active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+if [[ $bot_service == "running" ]]; then 
+   sts_bot="${g}[ON]${NC}"
+else
+   sts_bot="${r}[OFF]${NC}"
+fi
+
+rm -rf kyt.sh
+clear
+neofetch
+echo -e "  ${y} Your Data BOT Info"
+echo -e "  ${u}┌───────────────────────────────────┐${NC}"
+echo -e "  ${u}│$r Status BOT ${y}=$NC $sts_bot "
+echo -e "  ${u}│$r Token BOT  ${y}=$NC $bottoken "
+echo -e "  ${u}│$r Admin ID   ${y}=$NC $admin "
+echo -e "  ${u}│$r Domain     ${y}=$NC $domain "
+echo -e "  ${u}└───────────────────────────────────┘${NC}"
+echo -e ""
+history -c
+read -p "  Press [ Enter ] to back on menu"
+menu
