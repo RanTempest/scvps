6e6239795f9c74e2d861afd1d513040d3a41364d
RanTempest <151922808+RanTempest@users.noreply.github.com>
2024-03-02 23:08:55 -1200

    Update kyt.sh

--- kyt.sh
+++ kyt.sh
@@ -10,13 +10,13 @@ apt update && apt upgrade
 apt install neofetch -y
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/RanTempest/scvps/main/limit/bot.zip
+wget https://raw.githubusercontent.com/diah082/newbie/main/bot/kyt.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/RanTempest/scvps/main/limit/kyt.zip
+wget https://raw.githubusercontent.com/diah082/newbie/main/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
